48f6db0f3cbb410404580ffec2ec7b8af6a4cfd5
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-07-24 18:47:37 +0200

    created the start of the different tabs for calculations on the front page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,18 @@ function App() {
     <div className='mainContainer'>
       <div className='mainContent'>
         <h1>Running Buddy</h1>
+        <p>Calculations for your running needs</p>
+        <div className="calculationContainer">
+          <div className="leftContainer">
+            Distance
+          </div>
+          <div className="centerContainer">
+            Pace
+          </div>
+          <div className="rightContainer">
+            Time
+          </div>
+        </div>
       </div>
     </div>
   );
