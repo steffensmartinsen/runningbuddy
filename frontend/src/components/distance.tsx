d86b1b94d49b29e6aa4c7ece6db33622ed1d053b
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-08-03 13:56:31 +0200

    added radio buttons for km and miles in distance tab

--- frontend/src/components/distance.tsx
+++ frontend/src/components/distance.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import InputField from './inputField';
-import { Input } from '@chakra-ui/react';
+import { Input, RadioGroup, Radio, Stack } from '@chakra-ui/react';
 
 const DistanceHandler = () => {
+    const [metric, setMetric] = React.useState('km');
+    
+
     return (
         <>
             <h1 className="tabH1">Calculate Distance</h1>
             <p className="explanation">Provide the time of your run and the 
                 pace for the run in total to calculate the distance of the run.</p>
+            <p className="subTitle">Metric:</p>
+            <div className="radioContainer">
+                <RadioGroup onChange={setMetric} value={metric}>
+                    <Stack direction='column'>
+                        <Radio value='km' className="inputField">Km</Radio>
+                        <Radio value='miles' className="inputField">Miles</Radio>
+                    </Stack>
+                </RadioGroup>
+            </div>
             <p className="subTitle">Time:</p>
             <div className="timingsContainer">
                 <InputField placeholder='Hour' />
