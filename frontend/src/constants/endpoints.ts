4e323b786d1c17824f8f4779a906ae6ebc473be7
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-08-05 09:27:39 +0200

    refactored some text to consts

---
+++ frontend/src/constants/endpoints.ts
@@ -0,0 +1,14 @@
+// Endpoints to fetch data from the backend
+
+const LOCALHOST = "http://localhost";
+
+const PORT = "8080";
+
+const BASE_URL = `${LOCALHOST}:${PORT}/pace-calculator`;
+
+export const ENDPOINTS = {
+    DISTANCE: `${BASE_URL}/distance`,
+    SPECIFIEDDISTANCE: `${BASE_URL}/specified-distance`,
+    PACE: `${BASE_URL}/pace`,
+    TIME: `${BASE_URL}/time`
+}
---
+++ frontend/src/constants/http.ts
@@ -0,0 +1,19 @@
+
+export const HTTP_METHOD = {
+    GET: 'GET',
+    POST: 'POST',
+    OPTIONS: 'OPTIONS'
+}
+
+export const HTTP_STATUS = {
+    OK: 200,
+    NO_CONTENT: 204,
+    METHOD_NOT_ALLOWED: 405,
+    BAD_REQUEST: 400,
+    INTERNAL_SERVER_ERROR: 500
+}
+
+export const CONTENT_TYPE = {
+    JSON: 'application/json',
+    TEXT: 'text/plain'
+}
