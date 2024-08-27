c1b28fa2e2f60dab56d27a19311aea8864db4a50
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-08-27 10:22:20 +0200

    fixed bug with root redirect

--- backend/dist/routes/home.js
+++ backend/dist/routes/home.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Home = Home;
+const constants_1 = require("../constants");
 // Home is the function that serves the root path ('/')
 function Home(req, res) {
-    res.json({
-        message: "This path is not served. See the documentation for endpoints to invoke."
-    });
+    // Simply redirect to the pace calculator endpoint
+    res.redirect(308, constants_1.FRONTEND_URL + constants_1.ENDPOINT_PACE_CALCULATOR);
+    console.log(constants_1.FRONTEND_URL + constants_1.ENDPOINT_PACE_CALCULATOR);
 }
