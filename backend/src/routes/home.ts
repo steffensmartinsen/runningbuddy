b616b2c4a42adde7830e900d9e75bc9e20f9c9e2
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-08-27 10:42:24 +0200

    set CORS headers in Home function

--- backend/src/routes/home.ts
+++ backend/src/routes/home.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import { REDIRECT_URL } from '../constants';
+import { SetCORSHeaders } from '../helpers/functions';
 
 // Home is the function that serves the root path ('/')
 export function Home (req: Request, res: Response): void {
+
+    SetCORSHeaders(res);
 
     // Simply redirect to the pace calculator endpoint
     res.redirect(301, REDIRECT_URL);
