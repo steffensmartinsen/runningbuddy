dcc3dd4935c780727dcf09ce5956d96e202dc7c5
Steffen Sæthre Martinsen <steffema@stud.ntnu.no>
2024-08-27 11:25:08 +0200

    changed tab title

--- backend/dist/routes/home.js
+++ backend/dist/routes/home.js
@@ -4,6 +4,7 @@ exports.Home = Home;
 const constants_1 = require("../constants");
 // Home is the function that serves the root path ('/')
 function Home(req, res) {
-    res.send(`<p>The root path is not supported, please visit the pace calculator at </p><a href=${constants_1.REDIRECT_URL}>${constants_1.REDIRECT_URL}</a>`);
+    // res.send(`<p>The root path is not supported, please visit the pace calculator at </p><a href=${REDIRECT_URL}>${REDIRECT_URL}</a>`);
+    res.redirect(301, constants_1.REDIRECT_URL);
     console.log("The root path is not supported, please visit the pace calculator at " + constants_1.REDIRECT_URL);
 }
